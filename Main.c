7db73df3a8d3c00362da0767a8b4aea88c79e2e3
defalt <melancholic8078@gmail.com>
2025-03-19 16:52:03 +0200

    first try

--- Main.c
+++ Main.c
@@ -10,7 +10,7 @@ int main() {
 
     printf("Введіть коефіцієнт a: ");
     scanf("%lf", &a);
-    printf("Введіть початкове значення x_p: ");
+    printf("Введіть стартове значення x_p: ");
     scanf("%lf", &x_p);
     printf("Введіть кінцеве значення x_k: ");
     scanf("%lf", &x_k);
@@ -20,7 +20,7 @@ int main() {
     
     printf("\nРезультат:\n");
     for (double x = x_p; x <= x_k; x += dx) {
-        if (x > x_k) break;  // Додаємо умову для припинення циклу, якщо x перевищує x_k
+        if (x > x_k) break;  
         printf("Z(%.3f) = %.3f\n", x, Z(x, a));
     }
 
